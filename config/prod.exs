ff4e650122a73bd8839208e437d4b958d99d95fc
Josh Crews <josh@joshcrews.com>
2018-01-27 16:38:43 -0600

    heroku troubleshooinging

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,8 @@ use Mix.Config
 config :bcp, Bcp.Web.Endpoint,
   load_from_system_env: true,
   debug_errors: true,
+  url: [scheme: "https", host: "bcp-podcast.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   url: [host: "bcp-podcast.herokuapp.com"]
 
